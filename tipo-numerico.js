85d0f502b61690d0b4aa30ad6cf035b3abaa21b1
Uryel Farias <uryel.farias@gmail.com>
2025-04-17 11:21:55 -0300

    praticando tipos numericos.

---
+++ tipo-numerico.js
@@ -0,0 +1,9 @@
+const notaPrimeiroBi = 8;
+const noraSegundoBi = 6.3;
+const notaTerceiroBi = 5;
+const notaQuartoBi = parseInt('7');
+
+
+const total = notaPrimeiroBi + noraSegundoBi + notaTerceiroBi + notaQuartoBi; 
+
+console.log(total);
