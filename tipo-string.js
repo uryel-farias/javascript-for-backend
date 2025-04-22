2ed3ad9471be6a9a2a9dd107b709d768491b8403
Uryel Farias <uryel.farias@gmail.com>
2025-04-21 22:31:31 -0300

    template string

--- tipo-string.js
+++ tipo-string.js
@@ -4,3 +4,6 @@ const cumprimento = "Aprender e ensinar!";
 const citacao = `Julua diz: ${cumprimento}`
 
 console.log(citacao);
+
+console.log(`O nome da estudante é ${estudante}`);
+console.log(`O nome da professora é ${docente}`);
