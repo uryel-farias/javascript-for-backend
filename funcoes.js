4501283001053c1ef7822863ef2126503971659a
Uryel Farias <uryel.farias@gmail.com>
2025-04-24 13:08:35 -0300

    funções com parametros

--- funcoes.js
+++ funcoes.js
@@ -1,8 +1,9 @@
 // parâmetros/argumentos
 // retorno
 
-function exibeNomeEstudante(nome) {
-    console.log(nome);
-}
-
-exibeNomeEstudante('Caroline');
+function exibeInfosEstudante(nome, nota) {
+    return `o nome é ${nome} e a nota é ${nota}`;
+  }
+  
+  console.log(exibeInfosEstudante('Caroline', 10));
+  console.log(exibeInfosEstudante('Ana', 7));
